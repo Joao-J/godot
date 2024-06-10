db54ca48be118178f8a9989e4fae759550981805
JOÃO VITOR <118869806+Joao-J@users.noreply.github.com>
2024-06-10 16:58:12 -0300

    Add files via upload

---
+++ Player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var playeranimation: AnimationPlayer = $AnimationPlayer
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		playeranimation.play("andando")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	var directiony = Input.get_axis("ui_up", "ui_down")
+	if directiony:
+		playeranimation.play("andando")
+		velocity.y = directiony * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
+	
+	
